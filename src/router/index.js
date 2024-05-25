17033c419799a11329aa83b7042fdb9deeb7b07b
Ruth Kaz <ruth.kazembola@gmail.com>
2024-05-25 11:46:02 +0200

    Renamed paths and fixed bug!

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,9 @@ import AnmeldenView from '@/views/AnmeldenView.vue'
 import ArtistView from '@/views/ArtistView.vue'
 import CollectorView from '@/views/CollectorView.vue'
 import RegistrierenView from '@/views/RegistrierenView.vue'
+import FAQs from '@/views/FAQView.vue'
+import BasicKnowledge from '@/components/BasicKnowledge.vue'
+import ChatPageView from '@/views/ChatPageView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,35 +18,59 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+
     {
-      path: '/infoview',
+      path: '/intro',
       name: 'infoview',
       component: InfoView
     },
+
     {
-      path: '/anmeldenregistrieren',
+      path: '/welcome',
       name: 'anmeldenregistrieren',
       component: AnmeldenRegistrierenView
     },
+
     {
-      path: '/anmeldenview',
+      path: '/login',
       name: 'anmeldenview',
       component: AnmeldenView
     },
+
     {
-      path: '/artistview',
+      path: '/sign-up',
+      name: 'registierenview',
+      component: RegistrierenView
+    },
+
+    {
+      path: '/artist',
       name: 'artistview',
       component: ArtistView
     },
+
     {
-      path: '/collectorview',
+      path: '/collector',
       name: 'collectorview',
       component: CollectorView
     },
+
     {
-      path: '/registierenview',
-      name: 'registierenview',
-      component: RegistrierenView
+      path: '/chat',
+      name: 'chatpageview',
+      component: ChatPageView
+    },
+
+    {
+      path: '/faq',
+      name: 'FAQView',
+      component: FAQs
+    },
+
+    {
+      path: '/basic-knowledge',
+      name: 'BasicKnowledge',
+      component: BasicKnowledge
     }
   ]
 })
