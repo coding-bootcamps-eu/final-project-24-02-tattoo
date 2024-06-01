2999090488d4f61b50c006bd006d825fd5648f59
Bianca Dang <bncdng@gmail.com>
2024-06-01 13:52:52 +0200

    add some space for scrolling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <RouterView />
-  <NavBar />
+  <div class="app">
+    <router-link to="/infoview"> </router-link>
+    <RouterView />
+    <SliderComponent />
+    <NavBar />
+  </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import SliderComponent from './components/SliderComponent.vue'
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    SliderComponent
   }
 }
 </script>
+
+<style scoped>
+.app {
+  height: 200vh;
+}
+</style>
