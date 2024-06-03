b597d8c6253805c095811313b81f49af1484a0f4
Bianca Dang <bncdng@gmail.com>
2024-06-03 13:56:14 +0200

    add headline-sub and slider component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,24 +4,31 @@
   </header>
   <main>
     <ArtistCard />
+    <headline-sub title="Browse by Style"/>
+    <SliderComponent />
   </main>
 </template>
 
 <script>
 import ArtistCard from '@/components/ArtistCard.vue'
 import GlassHeader from '@/components/GlassHeader.vue'
+import SliderComponent from '@/components/SliderComponent.vue'
+import HeadlineSub from '@/components/HeadlineSub.vue'
 
 export default {
   name: 'HomeView',
   components: {
     ArtistCard,
-    GlassHeader
+    GlassHeader,
+    SliderComponent,
+    HeadlineSub
   }
 }
 </script>
 
 <style scoped>
 main {
+  height: 150vh;
   max-width: 90%;
   margin: auto;
 }
